59dec1dee53e4164dbae5824a67373fd1152554e
LouisPrepmyfuture <97509071+LouisPrepmyfuture@users.noreply.github.com>
2023-07-13 10:15:53 +0200

    last-commit

--- src/components/organisms/header/index.jsx
+++ src/components/organisms/header/index.jsx
@@ -3,57 +3,59 @@ import Image from '../../atoms/image'
 import logo from '../../../assets/img/logo/logo-kasa-color.png'
 import { NavLink } from 'react-router-dom'
 
-
 const NavContainer = styled.nav`
-  padding: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-	max-width: 1240px;
-	margin: 0 auto;
+ padding: 30px 0px;
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ max-width: 1240px;
+ margin: 0 auto;
+ @media (max-width: 683px) {
+  padding-top: 0px;
+ }
 `
 const ContentLink = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  > * {
-    &:last-child {
-      margin-left: 57px;
-			@media (max-width: 683px) {
-				margin-left: 20px;
-			}
-    }
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ > * {
+  &:last-child {
+   margin-left: 57px;
+   @media (max-width: 683px) {
+    margin-left: 20px;
+   }
   }
+ }
 `
 const StyledLink = styled(NavLink)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-	font-size: 24px;
-	font-weight: 500;
-	@media (max-width: 683px) {
-    font-size: 12px;
-		text-transform: uppercase;
-		.mobile{
-			width: 145px;
-			height: 47px;
-			border-radius: 0;
-		}
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ font-size: 24px;
+ font-weight: 500;
+ @media (max-width: 683px) {
+  font-size: 12px;
+  text-transform: uppercase;
+  .mobile {
+   width: 145px;
+   height: 47px;
+   border-radius: 0;
   }
+ }
 `
 
-function Header({classCss}) {
-  return (
-    <NavContainer className={classCss}>
-      <StyledLink>
-        <Image classCss="mobile" theme="logo" url={logo} alt="logo" />
-      </StyledLink>
-      <ContentLink id="sidebar" className="flexCenterCenter">
-        <StyledLink to="/" >Accueil</StyledLink>
-        <StyledLink to="/a-propos">A propos</StyledLink>
-      </ContentLink>
-    </NavContainer>
-  )
+function Header({ classCss }) {
+ return (
+  <NavContainer className={classCss}>
+   <StyledLink>
+    <Image classCss="mobile" theme="logo" url={logo} alt="logo" />
+   </StyledLink>
+   <ContentLink id="sidebar" className="flexCenterCenter">
+    <StyledLink to="/">Accueil</StyledLink>
+    <StyledLink to="/a-propos">A propos</StyledLink>
+   </ContentLink>
+  </NavContainer>
+ )
 }
 
 export default Header
